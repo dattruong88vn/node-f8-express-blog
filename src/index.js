ada88aa47f8242b1ceb66c03ca537e5ea1332f54
dat.truong <dat.truong@secomm.vn>
2023-10-13 22:40:11 +0700

    method post and parse body

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ const { engine } = require("express-handlebars");
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // static
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -28,6 +31,11 @@ app.get("/news", (req, res) => {
 app.get("/search", (req, res) => {
   console.log(req.query);
   res.render("search");
+});
+
+app.post("/search", (req, res) => {
+  console.log(req.body);
+  res.send("");
 });
 
 app.listen(port, () => {
